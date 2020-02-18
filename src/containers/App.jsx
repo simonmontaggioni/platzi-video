5e611ca9b9923b81a412497475f7e39be9ecdd6e
simonmontaggioni <simon.montaggioni@gmail.com>
2020-02-18 17:58:36 -0400

    Feat: Categories component, Carousel component and CarouselItem component added to the project, it's styles files were added too'

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import '../assets/styles/App.scss';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
 
 const App = () => (
     <div className="App">
         <Header></Header>
         <Search></Search>
+        <Categories>
+            <Carousel>
+                <CarouselItem></CarouselItem>
+                <CarouselItem></CarouselItem>
+                <CarouselItem></CarouselItem>
+                <CarouselItem></CarouselItem>
+                <CarouselItem></CarouselItem>
+            </Carousel>
+        </Categories>
     </div>
 );
 
