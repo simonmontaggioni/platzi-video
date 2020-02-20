f45ed71f1d46a16ffc7d4e3905f0310b74293daf
simonmontaggioni <simon.montaggioni@gmail.com>
2020-02-20 11:42:20 -0400

    Feat: implemented use of gravatar service

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from "react-router-dom";
 
+import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 import userIcon from '../assets/static/user-icon.png';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 
-const Header = () => (
-    <header className="header">
-        <Link to="/">
-            <img className="header__img" src={ logo } alt="Platzi Video"/>
-        </Link>
-        <div className="header__menu">
-            <div className="header__menu--profile">
-                <img src={ userIcon } alt=""/>
-                <p>Perfil</p>
-            </div>
-            <ul>
-                <li><a href="/">Cuenta</a></li>
-                <li>
-                    <Link to="/login"> Iniciar Sesión </Link>
-                </li>
-            </ul>
-        </div>
-    </header>
-);
+const Header = ( props ) => {
 
-export default Header;
+    const { user } = props;
+    const isUserLogged = Object.keys( user ).length > 0;
+
+    return (
+        <header className="header">
+            <Link to="/">
+                <img className="header__img" src={ logo } alt="Platzi Video"/>
+            </Link>
+            <div className="header__menu">
+                <div className="header__menu--profile">
+                    {
+                        isUserLogged 
+                        ?
+                        <img src={ gravatar( user.email ) } alt={ user.email }/>
+                        :
+                        <img src={ userIcon } alt=""/>
+                    }
+                    <p>Perfil</p>
+                </div>
+                <ul>
+                    <li><a href="/">Cuenta</a></li>
+                    <li>
+                        <Link to="/login"> Iniciar Sesión </Link>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    );
+
+};
+
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    };
+};
+
+export default connect ( mapStateToProps, null ) (Header);
